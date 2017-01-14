4cff2b1a0b6b78e98f8b6018a4bc5b4fa2728608
Selma Delgado <selmadelgado@gmail.com>
2017-01-13 18:41:58 -0600

    Update Personjs and Appjs to use Babel and ES6 Syntax

--- app/assets/scripts/App.js
+++ app/assets/scripts/App.js
@@ -1,8 +1,11 @@
 var $ = require('jquery');
+import Person from './modules/Person';
 
-var Person = require('./modules/Person');
-
-alert("ABC 321");
+class Adult extends Person {
+  payTaxes(){
+    console.log(this.name + " now owes $0 in taxes.");
+  }
+}
 
 var aries = new Person("Aries Peterson", "white");
 aries.greet();
@@ -10,8 +13,11 @@ aries.greet();
 var john = new Person("John Doe", "blue");
 john.greet();
 
-var jane = new Person("Jane Smith", "green");
+var jane = new Adult("Jane Smith", "orange");
 jane.greet();
+jane.payTaxes();
 
 
-/* $("h1").remove();*/
+/* $("h1").remove();
+
+alert("ABC 321"); */
