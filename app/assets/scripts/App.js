4ca2f72885c4f5b30559489948f06041cbab3b2d
Selma Delgado <selmadelgado@gmail.com>
2017-01-15 14:15:45 -0600

    Add Modal Lightbox Overlay

--- app/assets/scripts/App.js
+++ app/assets/scripts/App.js
@@ -1,11 +1,13 @@
 import $ from 'jquery';
+import Modal from './modules/Modal';
 import MobileMenu from './modules/MobileMenu';
 import RevealOnScroll from './modules/RevealOnScroll';
 import StickyHeader from './modules/StickyHeader';
 
 
+var modal = new Modal();
 var mobileMenu = new MobileMenu();
+var stickyHeader = new StickyHeader();
+
 new RevealOnScroll($(".feature-item"), "85%");
 new RevealOnScroll($(".testimonial"), "75%");
-
-var stickyHeader = new StickyHeader();
